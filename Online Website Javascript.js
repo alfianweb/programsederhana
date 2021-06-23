335a55e6bf988196311a699fe6497a370ee6f07e
alfianweb <84747585+alfianweb@users.noreply.github.com>
2021-06-23 11:46:59 +0700

    first commit

---
+++ Online Website Javascript.js
@@ -0,0 +1,24 @@
+alert('Hallo, selamat datang di alfianpedia');
+
+var ulang = true
+
+while(ulang === true) {
+    var nama = prompt('Masukkan nama anda :');
+    alert('Hallo ' + nama + ' terimakasih sudah berkunjung ke website kami');
+
+    ulang = confirm('Coba masukkan nama lagi? \n (klik cancel/tidak untuk tidak mengulangi) ');
+}
+
+alert('Terimakasih....'); 
+
+var angka = prompt('Masukkan angka :');
+
+if(angka % 2 === 0) {
+    alert(angka + ' adalah bilangan genap');
+} else if(angka % 2 === 1) {
+    alert(angka + ' adalah bilangan ganjil');
+} else {
+    alert('Yang anda masukkan bukan angka!!!')
+}
+
+alert('Oke terimakasih......');
